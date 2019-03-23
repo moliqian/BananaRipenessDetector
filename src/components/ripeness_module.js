e20c97a23621d801f45be698ea9e3e1a9404d6ba
Petar <kenicpetar@yahoo.com>
2019-03-23 14:09:48 -0400

    First commit

---
+++ src/components/ripeness_module.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+var serverURL = "localhost:5000/"
+
+export default class RipenessModule extends Component {
+    constructor(props){
+        super()
+        this.state = {
+
+        }
+
+        this.handle_file_input = this.handle_file_input.bind(this)
+    }
+    handle_file_input(e){
+        console.log(e.target.files[0])
+        let file = e.target.files[0]
+        if(file){
+            let data = new FormData()
+            data.append('file', file)
+            axios({
+                method: 'post',
+                url: serverURL,
+                data: data
+              }).then(function(response) {
+                  console.log("response!")
+                //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+              });
+        }
+    }
+    render() {
+        return (
+        <div className="App">
+            <form>
+                <h3>Import your image!</h3>
+                <input type="file" onChange={this.handle_file_input}/>
+            </form>
+        </div>
+        );
+    }
+}
+
+
