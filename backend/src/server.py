58929d7de577f8b5ffe41b74c7888656d3fd1c92
GV79 <glam01@uoguelph.ca>
2019-03-25 11:17:49 -0400

    text changes, optimizations made to threshold values and other values...

--- backend/src/server.py
+++ backend/src/server.py
@@ -18,7 +18,7 @@ import argparse
 import imutils
 import glob
 
-#Global Variable
+# Global Variables
 inputFilename = "Bananas.jpg"
 counter = 0
 bananaFound = 0  # 1 - true
@@ -37,40 +37,42 @@ def mse(imageA, imageB):
 	return err
 
 def checkRipeness(found, image, tW, tH):
-	(_, maxLoc, r) = found
-	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-	endY = endY-(int)((endY-startY)*0.05)
-	startY = startY+(int)((endY-startY)*0.05)
-	startX = startX + (int)((endX-startX)*0.05)
-	endX = endX - (int)((endX - startX) * 0.05)
-	banana = image[startY:endY, startX:endX]
+    (_, maxLoc, r) = found
+    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+    banana = image[startY:endY, startX:endX]
 
-	hsv = cv2.cvtColor(banana, cv2.COLOR_BGR2HSV)  # convert to HSV color space
-	mask = cv2.inRange(hsv, (17, 100, 0), (36, 255, 255))
-	dst = cv2.bitwise_and(banana, banana, mask=mask)
-	global counter
-	counter = counter + 1
-	#print(mse(banana, dst))
-	if mse(banana, dst) < 1000:  # if there is 1 found in the dst array, a red pixel as outlined by our boundaries exist
-		return True
-	else:
-		return False
+    hsv = cv2.cvtColor(banana, cv2.COLOR_BGR2HSV)  # convert to HSV color space
+    mask = cv2.inRange(hsv, (17, 50, 0), (36, 255, 255))
+    dst = cv2.bitwise_and(banana, banana, mask=mask)
+    # cv2.imshow("og", dst)
+    # cv2.imshow("og2", banana)
+    global counter
+    counter = counter + 1
+    # print("WOOHOO" + str(mse(banana, dst)))
+    if mse(banana, dst) < 5000:  # if yellow mask still very similar to original - ripe
+        return 1
+    else:
+        mask = cv2.inRange(hsv, (36, 50, 0), (70, 255, 255))  # green mask
+        dst = cv2.bitwise_and(banana, banana, mask=mask)
+        # cv2.imshow("bladh", dst)
+        # cv2.imshow("bladh2", banana)
+        print(mse(banana, dst))
+        if mse(banana, dst) < 3000:
+            return 2  # not ripe
+        else:
+            return 0  # overripe
+
 
 def imageFix(image):
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # convert to HSV color space
-	mask1 = cv2.inRange(hsv, (36, 50, 0), (70, 255, 255))  # green HSV mask
-	mask2 = cv2.inRange(hsv, (17, 100, 0), (36, 255, 255))  # yellow HSV mask
-	mask = cv2.bitwise_or(mask1, mask2)
-	mask3 = cv2.inRange(hsv, (0, 50, 0), (179, 255, 162))  # brown mask
-	mask = cv2.bitwise_or(mask, mask3)
-	dst = cv2.bitwise_and(image, image, mask=mask)  # generates image containing only yellow parts
-	return dst
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # convert to HSV color space
+    mask = cv2.inRange(hsv, (3, 28, 0), (81, 255, 255))  # mask that extracts yellow/brown/green colors (allows only acceptable banana colors)
+    dst = cv2.bitwise_and(image, image, mask=mask)  # generates image containing only yellow parts
+    return dst
 
 def identifyBanana(template, image):
-    # imageFix(template)
-    # cv2.imshow("Template", template)
-
+    global ripe
+    # cv2.imshow("test", template)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     template = cv2.Canny(template, 50, 200)
     (tH, tW) = template.shape[:2]
@@ -78,96 +80,67 @@ def identifyBanana(template, image):
     # load the image, convert it to gray-scale, and initialize the
     # bookkeeping variable to keep track of the matched region
     original = image
-    image = imageFix(image)
-    # cv2.imshow("image", image)
+    image = imageFix(image)  # get rid of most unnecessary colors except for yellow, brown, green colors
+    # cv2.imshow('blah', image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     found = None
 
-    # loop over the scales of the image
-    for scale in np.linspace(0.2, 1.0, 20)[::-1]:
-        # resize the image according to the scale, and keep track
-        # of the ratio of the resizing
-
+    # loop over the scales of the image changed from 0.2 to 0.1 to make certain images more accurate
+    for scale in np.linspace(0.1, 1.0, 20)[::-1]:
+        # resize the image according to the scale, and keep track of the ratio of the resizing
         resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
         r = gray.shape[1] / float(resized.shape[1])
 
-        # if the re-sized image is smaller than the template, then break
-        # from the loop
+        # if the re-sized image is smaller than the template, then return error image
         if resized.shape[0] < tH or resized.shape[1] < tW:
-            break
-        # detect edges in the re-sized, gray-scale image and apply template
-        # matching to find the template in the image
+            # image = cv2.imread('Images/imagefail.png')
+            # return image
+            break;
 
-        # cv2.imshow("resize", resized)
-
+        # detect edges in the re-sized, gray-scale image and apply template matching to find the template in the image
         edged = cv2.Canny(resized, 50, 200)
-        #cv2.imshow("blah", edged)
+        # cv2.imshow("edged", edged)
         result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-        # print(result)
         (minVal, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
 
-        # w, h = template.shape[::-1]
-        # threshold = 0.99
-        # loc = np.where(result >= threshold)
-        # for pt in zip(*loc[::-1]):
-        # 	cv2.rectangle(original, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-
-        # check to see if the iteration should be visualized
-        # if args.get("visualize", False):
-        #     # draw a bounding box around the detected region
-        #     clone = np.dstack([edged, edged, edged])
-        #     cv2.rectangle(clone, (maxLoc[0], maxLoc[1]), (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-        #     cv2.imshow("Visualize", clone)
-        #     cv2.waitKey(0)
-
-        # if we have found a new maximum correlation value, then update
-        # the bookkeeping variable
+        # if we have found a new maximum correlation value, then update the bookkeeping variable
         if (found is None or maxVal > found[0]) and maxVal > 3000000:
-            #print(minVal)
-            previousfound = found
+            # print('max: ' + str(maxVal))
             found = (maxVal, maxLoc, r)
-            if checkRipeness(found, image, tW, tH):  # Comment out this line and the two below to disable simple clothes color check
+            ripeness = checkRipeness(found, image, tW, tH)
+            if ripeness == 1:  # Comment out this line and the two below to disable simple clothes color check
                 ripe = 1
+            elif ripeness == 2:
+                ripe = 2
             else:
                 ripe = 0
-
-            # # exit(0)
-            # (_, maxLoc, r) = found
-            # (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-            # (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-            #
-            # # draw a bounding box around the detected result and display the image
-            # cv2.rectangle(original, (startX, startY), (endX, endY), (0, 0, 255), 2)
-            # cv2.putText(original, "Ripe", (int((startX + endX) / 2.2), int(startY + ((endY - startY) * 0.1))), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0, 0), 2)
-
-            # if not checkClothes(found, image):  # Comment out this line and the two below to disable simple clothes color check
-            # 	if previousfound is not None:
-            # 		found = previousfound
 
     # unpack the bookkeeping variable and compute the (x, y) coordinates
     # of the bounding box based on the re-sized ratio
 
     if found is None:
-        print('No banana found')
-        # exit(0)
-    (_, maxLoc, r) = found
-    (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-    (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
-
-    # draw a bounding box around the detected result and display the image
-    # cv2.rectangle(original, (startX, startY), (endX, endY), (0, 0, 255), 2)
-    if ripe == 1:
-        cv2.putText(original, "Ripe", (int((startX + endX)/2.2), int(startY+((endY-startY)*0.1))), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+        # print('No banana found')
+        h, w, d = original.shape
+        cv2.rectangle(original, (int(w/2)-100, int(h/4)-20), (int(w/2)+100, int(h/4)+10), (0,0,0), cv2.FILLED)
+        cv2.putText(original, "No banana found", (int(w/2)-95, int(h/4)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+        return original
     else:
-        print(startX)
-        print(endX)
-        cv2.putText(original, "Not ripe", (int((startX + endX) / 2.2), int(startY + ((endY - startY) * 0.1))), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-    # if not checkClothes(found, image):  # Comment out this line and the two below to disable simple clothes color check
-    # image = cv2.resize(image, (1400, 900))  # resize if necessary
-    # cv2.imshow("Image", original)
-    # cv2.imwrite('output.png', original)  # save image as output.png in source folder
-    # cv2.waitKey(0)
-    return original
+        (_, maxLoc, r) = found
+        (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+        (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+
+        # draw a bounding box around the detected result and display the image
+        cv2.rectangle(original, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        if ripe == 1:
+            cv2.putText(original, "Ripe", (int((startX + endX) / 2.2), int(startY + ((endY - startY) * 0.1))),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (40, 0, 255), 2)
+        elif ripe == 0:
+            cv2.putText(original, "Overripe", (int((startX + endX) / 2.2), int(startY + ((endY - startY) * 0.1))),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (40, 0, 255), 2)
+        else:
+            cv2.putText(original, "Unripe", (int((startX + endX) / 2.2), int(startY + ((endY - startY) * 0.1))),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (40, 0, 255), 2)
+        return original
     
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -183,7 +156,7 @@ def banana():
     
     newImage = identifyBanana(template, img)
     
-    #output file
+    #output file    
     cv2.imwrite("outputImage.jpg", newImage)
     return send_file("outputImage.jpg", mimetype='image/gif')
         
