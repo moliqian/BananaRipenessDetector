3ecfefc49bc099c65290e25a7fd87c27a6c026e3
Petar <kenicpetar@yahoo.com>
2019-03-24 20:42:16 -0400

    Fixed image not refreshing when a new one is uploaded

--- src/components/ripeness_module.js
+++ src/components/ripeness_module.js
@@ -7,7 +7,9 @@ export default class RipenessModule extends Component {
     constructor(props){
         super()
         this.state = {
-
+            original_url: "http://localhost:5000/banana?",
+            url: "http://localhost:5000/banana?",
+            index: 0
         }
 
         this.handle_file_input = this.handle_file_input.bind(this)
@@ -15,17 +17,31 @@ export default class RipenessModule extends Component {
     handle_file_input(e){
         console.log(e.target.files[0])
         let file = e.target.files[0]
+        let refresh = false
+        let render = false
+        let referenceToThis = this;
         if(file){
             let data = new FormData()
             data.append('file', file)
             axios({
                 method: 'post',
                 url: serverURL + "upload",
-                data: data
+                data: data,
               }).then(function(response) {
-                  console.log("response!")
+                  let today = new Date()
+                  let url = referenceToThis.state.url
+                  url = referenceToThis.state.original_url + today.getTime()
+                  referenceToThis.setState({
+                    url: url
+                  })
+                  
                 //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
               });
+        }
+        
+        if(refresh == true){
+            console.log("Jeez")
+            this.setState({})
         }
     }
     render() {
@@ -34,8 +50,9 @@ export default class RipenessModule extends Component {
             <form>
                 <h3>Import your image!</h3>
                 <input type="file" onChange={this.handle_file_input}/>
+                <div>{this.state.x}</div>
             </form>
-            <img style={{width: '300px'}} src="http://localhost:5000/banana"></img>
+            <img style={{width: '300px'}} src={this.state.url} ></img>
         </div>
         );
     }
