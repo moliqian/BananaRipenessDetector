b0db01ef3a53d4441382eda0936822a22ffef105
Lam <gl185108@ncr.com>
2019-05-01 17:01:25 -0400

    frontend design fixed

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import RipenessModule from "./components/ripeness_module"
+// import './home.css'
+
+const h1Style = {
+  fontSize: '5em',
+  color: '#ED9E46',
+  fontFamily: 'Montserrat',
+  marginTop: '25px'
+};
+
+const spanStyle = {
+  color: '#666666'
+};
 
 export default class Home extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Banana Ripeness Detector</h1>
+        <h1 style={h1Style}>Banana <span style={spanStyle}>Ripeness</span></h1>
         <RipenessModule/>
       </div>
     );
--- src/components/ripeness_module.js
+++ src/components/ripeness_module.js
@@ -1,14 +1,63 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import './ripeness.css';
 
-var serverURL = "http://localhost:5000/"
+var serverURL = "http://gv79.pythonanywhere.com/"
+
+const introStyle = {
+    fontFamily: 'Karla',
+
+    fontSize: '1.2em'
+};
+
+const divStyle = {
+    margin: '20px 28% 25px 28%',
+    paddingTop: '20px',
+    borderTop: '1px solid #c6c6c6'
+}
+
+const flexStyle = {
+    display: 'flex',
+    flex: '1',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+const formStyle = {
+    textAlign: 'center',
+    width: '15em',
+    fontSize: '1.2em',
+    margin: '1em'
+}
+
+const hrLine = {
+    borderTop: '1px solid #c6c6c6',
+    height: '5px',
+    margin: '15px 20% 0 20%'
+}
+
+const nameStyle = {
+    margin: '5px',
+    fontSize: '1.2em'
+}
+
+const iconStyle = {
+    margin: '2px',
+    fontSize: '2em'
+}
+
+const aS = {
+    textDecoration: 'none',
+    color: 'black'
+}
 
 export default class RipenessModule extends Component {
     constructor(props){
         super()
         this.state = {
-            original_url: "http://localhost:5000/banana?",
-            url: "http://localhost:5000/banana?",
+            original_url: "http://gv79.pythonanywhere.com/banana?",
+            url: "http://gv79.pythonanywhere.com/banana?",
             index: 0,
             loading: false
         }
@@ -51,16 +100,23 @@ export default class RipenessModule extends Component {
     render() {
         return (
         <div className="App">
-            <form>
-                <h3>Import your image!</h3>
-                <input type="file" onChange={this.handle_file_input}/>
-                <div>{this.state.x}</div>
-            </form>
+        <div style={divStyle}><p style={introStyle}>This web application computes the ripeness of a banana in an image. Upload your image below to test the algorithm out!</p></div>
             {this.state.loading ? (
                 <img src={require('../loading.gif')}/ >
             ) : 
-                <img style={{width: '800px', height: '500px', 'object-fit': 'contain'}} src={this.state.url}/ >
+                <img style={{width: '800px', height: '500px', 'objectFit': 'contain'}} src={this.state.url}/ >
             }
+            <form className="md-form">
+                <div style={flexStyle}>
+                    <div>{this.state.x}</div>
+                    <input type="file" className="form-control-file" style={formStyle} id="exampleFormControlFile1" onChange={this.handle_file_input}/>
+                </div>
+            </form>
+            <div style={hrLine}></div>
+            <div style={flexStyle}>
+                 <div style={nameStyle}><a style={aS} href="https://giavinhlam.me" target="_blank">Giavinh Lam</a> - <a style={aS} href="http://www.petarkenic.host/" target="_blank">Petar Kenic</a> - <a style={aS}>Greg Hetherington</a></div>
+                 <div style={iconStyle}><a style={aS} href="https://github.com/peken97/BananaRipenessDetector" target="_blank"><i className="fab fa-github"></i></a> <a style={aS} href="https://github.com/peken97/BananaRipenessDetector/blob/master/CIS%204720%20FINAL%20REPORT%20-%20IMAGE.pdf" target="_blank"><i class="fas fa-info-circle"></i></a></div>
+            </div>
         </div>
         );
     }
